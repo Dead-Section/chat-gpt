789b91ee08520a0451b648d2111698435f9f0a46
m-m-Code <118396777+m-m-Code@users.noreply.github.com>
2023-01-20 01:25:19 +0000

    updating App.jsx, updating node React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Footer, Blog, Possibilty, Features, WhatGPT3, Header} from "./containers"
 import { Brand, CTA, Navbar } from  "./components"
-import './App .css'
+import './App.css'
 
 function App(){
    return (
