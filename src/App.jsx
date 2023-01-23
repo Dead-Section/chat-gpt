7da22bcbe0b43f26724ca95f34a0c542c2810e75
hussein bahdon <35459369+Dead-Section@users.noreply.github.com>
2023-01-23 03:36:59 +0000

    updating App.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App(){
    return (
     <div className="App">   
-        <div classNmae="gradient__bg">
+        <div className="gradient__bg">
             <Navbar />
             <Header />
         </div>
