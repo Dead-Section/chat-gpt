1d0bb57af4ace9ede84e5d046929e1b38b6fe929
hussein bahdon <35459369+Dead-Section@users.noreply.github.com>
2023-01-24 02:35:51 +0000

    Create Menu

---
+++ src/components/navbar/Menu.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Menu () {
+
+    return (
+        <>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#WIGPT">What is GPT3</a></p>
+            <p><a href="#possibility">OpenAI</a></p>
+            <p><a href="#feature">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+        </>
+    )
+  
+    
+}
+
+
+
+export default Menu
