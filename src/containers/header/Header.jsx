59b4c82d43735017f6dc69f958f28715793ec8e2
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-09 15:19:28 +0000

    Summary ( Header.css, Header.jsx )

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,13 +3,15 @@ import People from '../../assets/people.png'
 import ai from '../../assets/ai.png'
 import './header.css';
 
+const text = "Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of."
+
 
 const Header = () => {
 return (
 <div className="gpt3__header section__padding" id="home">
-  <div classNmae="gpt3__header-content">
+  <div className="gpt3__header-content">
     <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-    <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+    <p>{text}</p>
 
 
     <div className="gpt3__header-content__input">
