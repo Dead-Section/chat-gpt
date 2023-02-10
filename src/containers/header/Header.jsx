c37f6dc70ac9849ce943222aabe719d48930d0cd
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-10 22:56:50 +0000

    updated header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,30 +6,29 @@ import './header.css';
 const text = "Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of."
 
 
-const Header = () => {
-return (
-<div className="gpt3__header section__padding" id="home">
-  <div className="gpt3__header-content">
-    <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-    <p>{text}</p>
+const paragrahp_Text = "Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of."
 
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>{paragrahp_Text} </p>
 
-    <div className="gpt3__header-content__input">
-      <input type="email" placeholder="Your Email Address" />
-      <button typ2="button">Get Started</button>
-    </div>
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
 
-    <div className="gpt3__header-content__people">
-      <img src={People} />
-      <p>1,600 people requested access a visit in last 24 hours</p>
+      <div className="gpt3__header-content__people">
+        <img src={People} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
     </div>
 
     <div className="gpt3__header-image">
-    <img src={ai} />
+      <img src={ai} />
     </div>
   </div>
-</div>
-)
-}
+);
 
 export default Header
