08511113bc0a1aa3610f424f33bb4b3f4af3c7fe
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-02-14 23:06:03 +0000

    Update index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
-export { default as Blog } from "./blog/Blog"
-export { default as Features } from "./features/Features"
-export { default as Footer } from "./footer/Footer"
-export { default as Header } from "./header/Header"
-export { default as Possibilty } from "./possibilty/Possibilty"
-export { default as WhatGPT3 } from "./whatGPT3/WhatGPT3"
+export { default as Blog } from "./blog/Blog" /* blog section*/
+export { default as Features } from "./features/Features" /* feature section*/
+export { default as Footer } from "./footer/Footer" /* footer section*/
+export { default as Header } from "./header/Header" /* header section*/
+export { default as Possibilty } from "./possibilty/Possibilty" /* possibilty section*/
+export { default as WhatGPT3 } from "./whatGPT3/WhatGPT3" /* whatsGPT3 section*/
